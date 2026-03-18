4403c8ed361cea06e38af646d4a57740738e5ec9
Anas <anas.aemd@gmail.com>
2026-03-18 15:12:26 +0200

    Configure CORS to allow requests from the Todo Client

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 const { todoRouter } = require("./routes/todo.js")
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: 'https://todo-client-3ii6.onrender.com'
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/", todoRouter)
